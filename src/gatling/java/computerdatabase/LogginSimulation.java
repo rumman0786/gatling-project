e4555635fa82bc8e0618774a2d6a30c495661486
Rumman Bin Ashraf <rumman@therapservices.net>
2022-11-30 16:08:07 +0600

    Updated gatling config for therap

--- src/gatling/java/computerdatabase/LogginSimulation.java
+++ src/gatling/java/computerdatabase/LogginSimulation.java
@@ -12,7 +12,7 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class LogginSimulation extends Simulation {
 
-    String baseUrl = "https://rumman-laptop.therapbd.net:7001";
+    String baseUrl = "https://rumman-laptop.therapbd.net:7002";
 
     String loginName = "zihad";
     String password = "therap321#";
@@ -29,22 +29,24 @@ public class LogginSimulation extends Simulation {
 //                    .formParam("providerCode", "#{providerCode}")
 //                    .formParam("password", password));
 
-    ChainBuilder loggerTest = exec(http("logger-test").get("/bt/loggerTest").check(status().is(200)));
+    ChainBuilder loggerTest = exec(http("logger-test")
+            .get("/bt/loggerTest")
+            .check(status().is(200)));
 
     HttpProtocolBuilder httpProtocol = http
-            .baseUrl(baseUrl);
-//            .contentTypeHeader("application/x-www-form-urlencoded")
-//            .acceptEncodingHeader("gzip, deflate, br")
-//            .connectionHeader("keep-alive")
-//            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-//            .userAgentHeader(
-//                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-//            );
+            .baseUrl(baseUrl)
+            .contentTypeHeader("application/x-www-form-urlencoded")
+            .acceptEncodingHeader("gzip, deflate, br")
+            .connectionHeader("keep-alive")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+            .userAgentHeader(
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+            );
 
     ScenarioBuilder loggerScenario = scenario("loggerScenario").exec(loggerTest);
 
     {
-        setUp(loggerScenario.injectOpen(rampUsers(100).during(10)))
+        setUp(loggerScenario.injectOpen(constantConcurrentUsers(100).during(10)))
                 .protocols(httpProtocol);
     }
 }
